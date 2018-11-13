c7c95ebf0c68e72bce62bc423d5716c8940b3b4e
gmailzj <gmailzj@qq.com>
2018-11-13 11:05:58 +0800

    go mod init "utils/uuid"

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ replace utils/uuid v0.0.0 => ./utils/uuid
 
 require (
 	github.com/eiblog/utils v0.0.0-20180918123929-fcdc03d4d492
+	github.com/google/uuid v1.0.0
 	utils/uuid v0.0.0
 )
--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 import "utils/uuid"
 import uuid2 "github.com/eiblog/utils/uuid"
+import uuid3 "github.com/google/uuid"
 
 func main() {
 	fmt.Println("Hello")
 	fmt.Println(uuid.NewV4())
 	fmt.Println(uuid2.NewV4())
+	var uuid = uuid3.New()
+	fmt.Println(uuid)
 }
